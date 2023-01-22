a8af1af233f8dea06df79eaae963192985edfc35
Md Zaid Siddique <87862008+Mdzaidsiddique@users.noreply.github.com>
2023-01-22 22:29:57 +0530

    Add files via upload
    Final commit

---
+++ puny-corn-6420/header.js
@@ -0,0 +1,14 @@
+
+// sticky nav
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("nav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
